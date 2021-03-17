28289f793d0b0e6398cc802332fc75e870ca8ad2
alejandropiuryp <alejandropiuryp@gmail.com>
2021-03-17 17:26:48 +0100

    Configuración multilenguaje del proyecto
    Se ha añadido la clase LocaleConfiguration.java para la configuración multilenguaje del proyecto con sus respectivas Beans.

--- src/main/java/org/springframework/samples/petclinic/configuration/WebConfig.java
+++ src/main/java/org/springframework/samples/petclinic/configuration/WebConfig.java
@@ -1,14 +1,7 @@
 package org.springframework.samples.petclinic.configuration;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.convert.ConversionService;
 import org.springframework.format.FormatterRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -34,4 +27,5 @@ public class WebConfig implements WebMvcConfigurer {
         resolver.setViewClass(JstlView.class);
         registry.viewResolver(resolver);
     }
+    
 }
