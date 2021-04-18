f395e18f40d0b92d9fa4f490155a103f87b29e8a
Enrique Barba <35747594+enriquebarba97@users.noreply.github.com>
2021-04-18 17:36:17 +0200

    CauseService añadido con funciones para gestionar Cause y Donation

--- src/main/java/org/springframework/samples/petclinic/model/Donation.java
+++ src/main/java/org/springframework/samples/petclinic/model/Donation.java
@@ -2,9 +2,7 @@ package org.springframework.samples.petclinic.model;
 
 import java.time.LocalDate;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 
 import org.springframework.format.annotation.DateTimeFormat;
@@ -17,11 +15,15 @@ import lombok.Setter;
 @Entity
 @Table(name="donations")
 public class Donation extends BaseEntity{
-	
+
+    @ManyToOne
+    @JoinColumn(name = "cause_id")
+    private Cause cause;
+
 	@Column(name="donation_date")
 	@DateTimeFormat(pattern= "yyyy/MM/dd")
 	private LocalDate date;
-	
+
 	@NotNull
 	@Column(name="amount")
 	private Double amount;
